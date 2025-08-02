e3d1f13c148cf2da4fa2d9d29a24cc0c8b7dd102
Ruban Daniil <danya.ruban.2005@mail.ru>
2025-08-02 14:15:40 +0300

    feat/dev-0/replace image of hand

--- src/pages/registration/components/logo/Logo.tsx
+++ src/pages/registration/components/logo/Logo.tsx
@@ -6,7 +6,14 @@ export const Logo = () => {
   const { name } = useSelector((state: RootState) => state.user)
   return (
     <div className="logo block-animation">
-      <p className="logo-animation-start logo-animation logo-picture">&#9995;</p>
+      <img
+        className="logo-animation-start logo-animation logo-picture"
+        src="/images/hand.png"
+        alt="Logo"
+      />
+      {/* <p className="logo-animation-start logo-animation" style={{ fontSize: '20rem', margin: '0' }}>
+        &#9995;
+      </p> */}
       <p className="text-animation logo-p">Привет, {name}</p>
     </div>
   )
