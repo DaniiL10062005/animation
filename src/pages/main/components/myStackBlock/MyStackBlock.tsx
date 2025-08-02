2c7a80bdef200761807ee4cd3192d340bf256efb
Ruban Daniil <danya.ruban.2005@mail.ru>
2025-08-02 13:54:27 +0300

    feat/dev-0/change image path 2.0

--- src/pages/main/components/myStackBlock/MyStackBlock.tsx
+++ src/pages/main/components/myStackBlock/MyStackBlock.tsx
@@ -6,29 +6,29 @@ export const MyStackBlock = () => {
     <Block className="my-stack-block">
       <p className="header">Мой стек технологий</p>
       <div className="stack-container react">
-        <img src="/public/images/react.png" alt="React" className="stack-image" />
+        <img src="/images/react.png" alt="React" className="stack-image" />
         <p>
           React (Hooks, Context, react-router, react-hook-form, react-i18next, react-redux), React
           Native
         </p>
       </div>
       <div className="stack-container">
-        <img src="/public/images/html.png" alt="HTML" className="stack-image" />
-        <img src="/public/images/js.png" alt="JavaScript" className="stack-image" />
-        <img src="/public/images/css.png" alt="CSS" className="stack-image" />
+        <img src="/images/html.png" alt="HTML" className="stack-image" />
+        <img src="/images/js.png" alt="JavaScript" className="stack-image" />
+        <img src="/images/css.png" alt="CSS" className="stack-image" />
         <p>JavaScript, TypeScript, HTML, CSS</p>
       </div>
       <div className="stack-container git">
-        <img src="/public/images/git.png" alt="Git" className="stack-image" />
-        <img src="/public/images/github.png" alt="GitHub" className="stack-image" />
+        <img src="/images/git.png" alt="Git" className="stack-image" />
+        <img src="/images/github.png" alt="GitHub" className="stack-image" />
         <p>Git, GitHub</p>
       </div>
       <div className="stack-container tailwind">
-        <img src="/public/images/tailwind.png" alt="Tailwind" className="stack-image" />
+        <img src="/images/tailwind.png" alt="Tailwind" className="stack-image" />
         <p>Axios, MUI, ANTDesign, React Native Paper, Zustand, zod, Tailwind, Fetch</p>
       </div>
       <div className="stack-container sql">
-        <img src="/public/images/postgresql.png" alt="PostgreSQL" className="stack-image" />
+        <img src="/images/postgresql.png" alt="PostgreSQL" className="stack-image" />
         <p>PostgreSQL</p>
       </div>
     </Block>
